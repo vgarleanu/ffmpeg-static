9bb227ee850dcc3230ffbfb4a82ae6c71c871ad4
Leon Handreke <leonh@ndreke.de>
2018-11-12 21:37:19 +0100

    Re-add ubuntu-deps.sh file for CI

---
+++ ubuntu-deps.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+apt-get update
+apt-get install build-essential curl tar pkg-config
+apt-get -y --force-yes install \
+  autoconf \
+  automake \
+  build-essential \
+  cmake \
+  frei0r-plugins-dev \
+  gawk \
+  libass-dev \
+  libfreetype6-dev \
+  libopencore-amrnb-dev \
+  libopencore-amrwb-dev \
+  libsdl1.2-dev \
+  libspeex-dev \
+  libssl-dev \
+  libtheora-dev \
+  libtool \
+  libva-dev \
+  libvdpau-dev \
+  libvo-amrwbenc-dev \
+  libvorbis-dev \
+  libwebp-dev \
+  libxcb1-dev \
+  libxcb-shm0-dev \
+  libxcb-xfixes0-dev \
+  libxvidcore-dev \
+  pkg-config \
+  texi2html \
+  zlib1g-dev \
+  git \
+  wget
